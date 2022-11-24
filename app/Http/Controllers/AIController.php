5f5d06011dac875bd603e306a857af9c75af875c
Gempita Rizki (R) ? <38187462+GempitaRizki@users.noreply.github.com>
2022-11-25 00:19:49 +0700

    connect the OpenAi API KEY

--- app/Http/Controllers/AIController.php
+++ app/Http/Controllers/AIController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Orhanerday\OpenAi\OpenAi;
 
 class AIController extends Controller
 {
@@ -10,7 +11,22 @@ class AIController extends Controller
         return view('ai.index');
     }
 
-    public function result(){
+    public function result(Request $request){
+
+        $kalimat = $request->kalimat;
+
+        $open_ai = new OpenAi(env('OPEN_AI_API_KEY'));
+        $prompt = "Membuat Kalimat Palsu dari sebuah kata";
+
+        $openAiOutput = $open_ai->complete([
+            'engine' => 'davinci-instruct-beta-v3',
+            'prompt' => 'Hello',
+            'temperature' => 0.9,
+            'max_tokens' => 150,
+            'frequency_penalty' => 0,
+            'presence_penalty' => 0.6,
+        ]);
+
         return view('ai.index', ['result' => 'Setiap kata 
         ter-generate menggunakan AI']);
     }
