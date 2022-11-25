655516e8dc11b25a8ab73d5d560efa7243672368
Gempita Rizki (R) ? <38187462+GempitaRizki@users.noreply.github.com>
2022-11-26 02:03:39 +0700

    input some text as a prompt

--- app/Http/Controllers/AIController.php
+++ app/Http/Controllers/AIController.php
@@ -21,16 +21,19 @@ class AIController extends Controller
 
         $openAiOutput = $open_ai->complete([
             'engine' => 'davinci-instruct-beta-v3',
-            'prompt' => 'Hello',
+            'prompt' => $prompt,
             'temperature' => 0.9,
             'max_tokens' => 150,
             'frequency_penalty' => 0,
             'presence_penalty' => 0.6,
         ]);
 
+
         $output = json_decode($openAiOutput, true);
         $outputText = $output["choices"][0]["text"];
 
         return view('ai.index', ['result' => $outputText ]);
+
+        
     }
 }
